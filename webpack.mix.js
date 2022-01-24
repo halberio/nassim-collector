eaa544aac5ebb438ff00b487ebb6a53b307bcfca
Hamza Ayech <4yech.hamza@gmail.com>
2022-01-24 19:33:54 +0100

    Prod bundle

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require("laravel-mix");
 require("laravel-mix-react-typescript-extension");
-mix.reactTypeScript("resources/js/index.tsx", "public/js")
-    .sass("resources/sass/app.scss", "public/css")
-    .browserSync("http://localhost:8000");
+mix
+  .reactTypeScript("resources/js/index.tsx", "public/js")
+  .version()
+  .sass("resources/sass/app.scss", "public/css")
+  .browserSync("http://localhost:8000");
