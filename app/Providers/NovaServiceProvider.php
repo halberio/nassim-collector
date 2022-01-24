7b8e76005ca00bb3c640a51925d61366d37d36e2
Hamza Ayech <4yech.hamza@gmail.com>
2022-01-24 19:29:47 +0100

    Prod bundle

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -47,7 +47,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
   {
     Gate::define('viewNova', function ($user) {
       return in_array($user->email, [
-        //
+        "admin@shadacollector.com"
       ]);
     });
   }
