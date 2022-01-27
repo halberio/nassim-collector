a7407bf1b822e3d42d0bd94991ba3f79c42373c2
Hamza Ayech <4yech.hamza@gmail.com>
2022-01-27 14:30:31 +0100

    Analytics

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,45 +1,59 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="theme-color" content="#9bdcfe">
-    <title>Shada Collector</title>
-    <link rel="stylesheet" href={{ asset('css/app.css') }}>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-<!-- Facebook Pixel Code -->
-<script>
-  !function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window,document,'script',
-  'connect.facebook.net/en_US/fbevents.js');
-   fbq('init', '810517026214568');
-  fbq('track', 'PageView');
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="theme-color" content="#9bdcfe">
+  <title>Shada Collector</title>
+  <link rel="stylesheet" href={{ asset('css/app.css') }}>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <!-- Facebook Pixel Code -->
+  <script>
+    ! function(f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function() {
+        n.callMethod ?
+          n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = '2.0';
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = !0;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s)
+    }(window, document, 'script',
+      'connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '810517026214568');
+    fbq('track', 'PageView');
   </script>
   <noscript>
-   <img height="1" width="1"
-  src="facebook.com/tr?id=810517026214568&ev=PageView
-  &noscript=1"/>
+    <img height="1" width="1" src="facebook.com/tr?id=810517026214568&ev=PageView
+  &noscript=1" />
   </noscript>
-  <!-- End Facebook Pixel Code -->
   <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-VSRS49JH1P"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-6SJL5D23G4"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
 
-  gtag('config', 'G-VSRS49JH1P');
-</script>
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-6SJL5D23G4');
+  </script>
 </head>
-<body >
-<div id="root"></div>
-<script src="{{ asset('js/index.js') }}" defer></script>
+
+<body>
+  <div id="root"></div>
+  <script src="{{ asset('js/index.js') }}" defer></script>
 
 </body>
+
 </html>
